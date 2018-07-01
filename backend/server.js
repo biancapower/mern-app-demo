52722dd6ba32754876407b9c3d4b50c247ae9643
Bianca Power <drbiancampower@gmail.com>
2018-07-01 10:33:57 +1000

    create express server (with no functionality)
    to start the server:
    mern-demo/backend$ yarn start
    
    to make a request to the server go to:
    http://localhost:7000/
    
    when loading in browser you should get an error 'Cannot GET /'

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,5 @@
-console.log("Server started...");
+const express = require('express');
+const server = express();
+
+const PORT = 7000;
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
